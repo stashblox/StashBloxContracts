c4ea4b62cf427bf11298d17cf454468632d72c30
Ouziel Slama <ouziel@stashblox.com>
2020-10-14 11:11:08 +0200

    clean unused files

---
+++ utils/gendoc.sh
@@ -0,0 +1,1 @@
+node dist/cli.js -i ../StashBloxContracts/contracts -o ../StashBloxContracts/docs -t ../StashBloxContracts/scripts/ --solc-module solc-0.7 -s single -e ../StashBloxContracts/contracts/lib/utils
