d2def3f64c17189f93c539a80cfc2820b0827439
Ouziel Slama <ouziel@stashblox.com>
2020-10-21 22:02:25 +0200

    Add Withdrawable.sol

--- contracts/StashBlox.sol
+++ contracts/StashBlox.sol
@@ -2,11 +2,11 @@
 pragma solidity ^0.7.1;
 
 import './interfaces/IERC1155Metadata.sol';
-import "./utils/SafeMath.sol";
 import './utils/StringUtils.sol';
 import './lib/Callable.sol';
+import './lib/Withdrawable.sol';
 
-contract StashBlox is Callable, IERC1155Metadata, StringUtils {
+contract StashBlox is Callable, Withdrawable, IERC1155Metadata, StringUtils {
 
     using SafeMath for uint256;
 
@@ -17,33 +17,6 @@ contract StashBlox is Callable, IERC1155Metadata, StringUtils {
         _config.callbackAutoExecuteMaxAddresses = 50;
         _config.baseURI = "http://stashblox.com/tokens/";
         _transferOwnership(msg.sender);
-    }
-
-    /**
-     * @dev Function to withdraw ETH from the contract.
-     * @param to recipent address
-     * @param amount amount to withdraw
-     */
-    function withdraw(address to, uint256 amount) external onlyOwner {
-        require(_users[to].ETHBalance >= amount, "Insufficient balance");
-        (bool success, ) = to.call{value: amount}("");
-        require(success, "Withdrawal failed");
-        _users[to].ETHBalance -= amount;
-    }
-
-    /**
-     * @dev Function to make an ETH deposit that can be used to pay token transfer .
-     * @param to recipent address
-     */
-    function deposit(address to) external payable {
-        _users[to].ETHBalance = _users[to].ETHBalance.add(msg.value);
-    }
-
-    /**
-     * @dev Receive Ether Function:this is the function that is executed on plain Ether transfers (e.g. via .send() or .transfer()).
-     */
-    receive() external payable {
-        _users[msg.sender].ETHBalance = _users[msg.sender].ETHBalance.add(msg.value);
     }
 
     /**
---
+++ contracts/lib/Withdrawable.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.7.1;
+
+import "../utils/SafeMath.sol";
+import './Data.sol';
+
+contract Withdrawable is Data {
+
+    using SafeMath for uint256;
+
+
+    /****************************
+    EXTERNAL FUNCTIONS
+    *****************************/
+
+
+    /**
+     * @dev Function to withdraw ETH from the contract. TODO: Add permission ??
+     * @param to recipent address
+     * @param amount amount to withdraw
+     */
+    function withdraw(address to, uint256 amount) external {
+        require(_users[to].ETHBalance >= amount, "Insufficient balance");
+        (bool success, ) = to.call{value: amount}("");
+        require(success, "Withdrawal failed");
+        _users[to].ETHBalance -= amount;
+    }
+
+    /**
+     * @dev Function to make an ETH deposit that can be used to pay token transfer .
+     * @param to recipent address
+     */
+    function deposit(address to) external payable {
+        _users[to].ETHBalance = _users[to].ETHBalance.add(msg.value);
+    }
+
+    /**
+     * @dev Receive Ether Function:this is the function that is executed on plain Ether transfers (e.g. via .send() or .transfer()).
+     */
+    receive() external payable {
+        _users[msg.sender].ETHBalance = _users[msg.sender].ETHBalance.add(msg.value);
+    }
+
+
+}
