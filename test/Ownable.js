87b2af6207605a1d56047d9f6b2df5051ebca008
Ouziel Slama <ouziel@stashblox.com>
2020-10-25 16:50:03 +0100

    add test for Ownable.sol

---
+++ test/Ownable.js
@@ -0,0 +1,56 @@
+const {
+  initContract,
+  initFixtures,
+  transferTokens,
+  bigN,
+  random,
+  accounts,
+  expectEvent,
+  expectRevert,
+  assert,
+  defaultSender,
+  ZERO_ADDRESS
+} = require("./lib/helpers.js");
+
+describe("Ownable.sol", () => {
+
+  beforeEach(async function () {
+    STASHBLOX = await initContract();
+    DATA = await initFixtures();
+  });
+
+  describe("#owner", async () => {
+
+    it("should return correct owner", async () => {
+      const owner = await STASHBLOX.owner();
+      assert.equal(owner, defaultSender, "Invalid owner");
+
+      const config = await STASHBLOX._config();
+      assert.equal(config.owner, defaultSender, "Invalid owner");
+
+    });
+
+  });
+
+
+  describe("#transferOwnership", async () => {
+
+    it("should change the owner", async () => {
+      let owner = await STASHBLOX.owner();
+      assert.equal(owner, defaultSender, "Invalid owner");
+
+      const receipt = await STASHBLOX.transferOwnership(accounts[3]);
+
+      expectEvent(receipt, "OwnershipTransferred", {
+        previousOwner: defaultSender,
+        newOwner: accounts[3]
+      })
+
+      owner = await STASHBLOX.owner();
+      assert.equal(owner, accounts[3], "Invalid owner");
+    });
+
+  });
+
+
+});
