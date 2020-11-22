447be617fbc4fd42678090871252a3a74c35af41
Ouziel Slama <ouziel@stashblox.com>
2020-11-22 18:24:34 +0100

    add clone factory

--- contracts/lib/Core/Core.sol
+++ contracts/lib/Core/Core.sol
@@ -4,5 +4,6 @@ pragma solidity ^0.7.4;
 import "./Data.sol";
 import "./GSNCapable.sol";
 import "./Authorizable.sol";
+import "./CloneFactory.sol";
 
-contract Core is Data, GSNCapable, Authorizable { } // solhint-disable-line no-empty-blocks
+contract Core is Data, GSNCapable, Authorizable, CloneFactory { } // solhint-disable-line no-empty-blocks
